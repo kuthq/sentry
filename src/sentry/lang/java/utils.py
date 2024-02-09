766a0cb75665b820c686f8fdffe9aca5a5fbbaf6
Sebastian Zivota <loewenheim@mailbox.org>
2024-02-09 15:04:06 +0100

    Fix any call

--- src/sentry/lang/java/utils.py
+++ src/sentry/lang/java/utils.py
@@ -28,7 +28,7 @@ def has_proguard_file(data):
     Checks whether an event contains a proguard file
     """
     images = get_path(data, "debug_meta", "images", filter=True)
-    return any(images, is_valid_proguard_image)
+    return any(map(is_valid_proguard_image, images))
 
 
 def get_proguard_images(event: dict[str, Any]) -> set[str]:
